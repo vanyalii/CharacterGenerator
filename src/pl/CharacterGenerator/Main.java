9d13fdff3c644f83e547e548913d1bda32dbe306
vanyalii <adrianrozek5@gmail.com>
2020-02-16 22:38:12 +0100

    generowanie do pliku

--- src/pl/CharacterGenerator/Main.java
+++ src/pl/CharacterGenerator/Main.java
@@ -1,5 +1,9 @@
 package pl.CharacterGenerator;
 
+import javax.imageio.IIOException;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
@@ -7,7 +11,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Logger log = Logger.getLogger("log.txt");
 
         Scanner sc = new Scanner(System.in);
@@ -34,33 +38,34 @@ public class Main {
 
         String newline = System.lineSeparator();
 
-        switch (choiceSex){
-            case 1:{
+        String maleName = null;
+        String femaleName = null;
+        switch (choiceSex) {
+            case 1: {
                 log.log(Level.INFO, "Generowanie męskiego imienia");
                 try {
                     TimeUnit.SECONDS.sleep(3);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                String maleName = Names.getMaleName();
+                maleName = Names.getMaleName();
                 System.out.println("Twoje imię to: " + maleName + newline);
 
                 break;
             }
-            case 2:{
-
-                System.out.println("Randomowe generowanie męskiego imienia...");
+            case 2: {
+                log.log(Level.INFO, "Generowanie damskiego imienia");
                 try {
                     TimeUnit.SECONDS.sleep(3);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                String maleName = Names.getFemaleName();
-                System.out.println("Twoje imię to: " + maleName + newline);
+                femaleName = Names.getFemaleName();
+                System.out.println("Twoje imię to: " + femaleName + newline);
                 break;
             }
         }
-        log.log(Level.INFO,"Wybór klasy");
+        log.log(Level.INFO, "Wybór klasy");
         System.out.println("Wybierz rase:");
         System.out.println("1: Elf");
         System.out.println("2: Człowiek");
@@ -68,10 +73,8 @@ public class Main {
         int choiceRace = sc.nextInt();
 
 
-
-        switch(choiceRace){
-            case 1:
-            {
+        switch (choiceRace) {
+            case 1: {
                 attributsResault.add(diceGeneratorResaultTo20WW + elf.WW);
                 attributsResault.add(diceGeneratorResaultTo20US + elf.US);
                 attributsResault.add(diceGeneratorResaultTo20K + elf.K);
@@ -82,45 +85,68 @@ public class Main {
                 attributsResault.add(diceGeneratorResaultTo20OGD + elf.OGD);
                 attributsResault.add(elf.SZ);
                 int S = attributsResault.get(2);
-                int strongResault = S/10;
+                int strongResault = S / 10;
                 int WT = attributsResault.get(3);
-                int resistanceResault = WT/10;
+                int resistanceResault = WT / 10;
                 attributsResault.add(strongResault);
                 attributsResault.add(resistanceResault);
                 attributsResault.add(elf.ZYW());
                 attributsResault.add(elf.PP());
 
-                log.log(Level.INFO,"generowanie atrybutów");
+                log.log(Level.INFO, "generowanie atrybutów");
 
-                System.out.println("Oto wylosowane atrybuty Elfa: " + newline + "Twoja walka wręcz: " + attributsResault.get(0) + newline + "Twoje umiejętności strzeleckie: " + attributsResault.get(1) + newline + "Twoja krzepa: " + attributsResault.get(2) + newline + "Twoja odporność: " + attributsResault.get(3) + newline + "Twoja zręczność: " + attributsResault.get(4) + newline + "Twoja inteligencja: " + attributsResault.get(5) + newline + "Twoja siła woli: " + attributsResault.get(6) + newline + "Twoja ogłada: " + attributsResault.get(7) + newline + "Twoja szybkość: " + attributsResault.get(8) + newline + "Twoja siła: " + attributsResault.get(9) + newline + "Twoja odporność: " + attributsResault.get(10) + newline + "Twoja żywotność: " + attributsResault.get(11) + newline + "Twoje punkty przeznaczenia: " + attributsResault.get(12));
+                System.out.println("Oto wylosowane atrybuty Elfa: " + newline + "Twoja walka wręcz: " + attributsResault.get(0) + newline + "Twoje umiejętności strzeleckie: " + attributsResault.get(1) + newline + "Twoja krzepa: " + attributsResault.get(2) + newline + "Twoja odporność: " + attributsResault.get(3) + newline + "Twoja zręczność: " + attributsResault.get(4) + newline + "Twoja inteligencja: " + attributsResault.get(5) + newline + "Twoja siła woli: " + attributsResault.get(6) + newline + "Twoja ogłada: " + attributsResault.get(7) + newline + "Twoja szybkość: " + attributsResault.get(8) + newline + "Twoja siła: " + attributsResault.get(9) + newline + "Twoja wytrzymałość: " + attributsResault.get(10) + newline + "Twoja żywotność: " + attributsResault.get(11) + newline + "Twoje punkty przeznaczenia: " + attributsResault.get(12));
                 break;
             }
-                case 2: {
-                    System.out.println(diceGeneratorResaultTo20INT);
-                    attributsResault.add(diceGeneratorResaultTo20WW + human.WW);
-                    attributsResault.add(diceGeneratorResaultTo20US + human.US);
-                    attributsResault.add(diceGeneratorResaultTo20K + human.K);
-                    attributsResault.add(diceGeneratorResaultTo20ODP + human.ODP);
-                    attributsResault.add(diceGeneratorResaultTo20ZR + human.ZR);
-                    attributsResault.add(diceGeneratorResaultTo20INT + human.INT);
-                    attributsResault.add(diceGeneratorResaultTo20SW + human.SW);
-                    attributsResault.add(diceGeneratorResaultTo20OGD + human.OGD);
-                    attributsResault.add(human.SZ);
-                    int S = attributsResault.get(2);
-                    int strongResault = S / 10;
-                    int WT = attributsResault.get(3);
-                    int resistanceResault = WT / 10;
-                    attributsResault.add(strongResault);
-                    attributsResault.add(resistanceResault);
-                    attributsResault.add(human.ZYW());
-                    attributsResault.add(human.PP());
+            case 2: {
+                System.out.println(diceGeneratorResaultTo20INT);
+                attributsResault.add(diceGeneratorResaultTo20WW + human.WW);
+                attributsResault.add(diceGeneratorResaultTo20US + human.US);
+                attributsResault.add(diceGeneratorResaultTo20K + human.K);
+                attributsResault.add(diceGeneratorResaultTo20ODP + human.ODP);
+                attributsResault.add(diceGeneratorResaultTo20ZR + human.ZR);
+                attributsResault.add(diceGeneratorResaultTo20INT + human.INT);
+                attributsResault.add(diceGeneratorResaultTo20SW + human.SW);
+                attributsResault.add(diceGeneratorResaultTo20OGD + human.OGD);
+                attributsResault.add(human.SZ);
+                int S = attributsResault.get(2);
+                int strongResault = S / 10;
+                int WT = attributsResault.get(3);
+                int resistanceResault = WT / 10;
+                attributsResault.add(strongResault);
+                attributsResault.add(resistanceResault);
+                attributsResault.add(human.ZYW());
+                attributsResault.add(human.PP());
 
-                    System.out.println("Oto wylosowane atrybuty Człowieka: " + newline + "Twoja walka wręcz: " + attributsResault.get(0) + newline + "Twoje umiejętności strzeleckie: " + attributsResault.get(1) + newline + "Twoja krzepa: " + attributsResault.get(2) + newline + "Twoja odporność: " + attributsResault.get(3) + newline + "Twoja zręczność: " + attributsResault.get(4) + newline + "Twoja inteligencja: " + attributsResault.get(5) + newline + "Twoja siła woli: " + attributsResault.get(6) + newline + "Twoja ogłada: " + attributsResault.get(7) + newline + "Twoja szybkość: " + attributsResault.get(8) + newline + "Twoja siła: " + attributsResault.get(9) + newline + "Twoja odporność: " + attributsResault.get(10) + newline + "Twoja żywotność: " + attributsResault.get(11) + newline + "Twoje punkty przeznaczenia: " + attributsResault.get(12));
+                System.out.println("Oto wylosowane atrybuty Człowieka: " + newline + "Twoja walka wręcz: " + attributsResault.get(0) + newline + "Twoje umiejętności strzeleckie: " + attributsResault.get(1) + newline + "Twoja krzepa: " + attributsResault.get(2) + newline + "Twoja odporność: " + attributsResault.get(3) + newline + "Twoja zręczność: " + attributsResault.get(4) + newline + "Twoja inteligencja: " + attributsResault.get(5) + newline + "Twoja siła woli: " + attributsResault.get(6) + newline + "Twoja ogłada: " + attributsResault.get(7) + newline + "Twoja szybkość: " + attributsResault.get(8) + newline + "Twoja siła: " + attributsResault.get(9) + newline + "Twoja wytrzymałość: " + attributsResault.get(10) + newline + "Twoja żywotność: " + attributsResault.get(11) + newline + "Twoje punkty przeznaczenia: " + attributsResault.get(12));
                 break;
 
             }
         }
+        if (choiceSex == 1){
+            String writeToFile = "Twoje imię to: " + maleName + newline + "Oto wylosowane atrybuty Elfa: " + newline + "Twoja walka wręcz: " + attributsResault.get(0) + newline + "Twoje umiejętności strzeleckie: " + attributsResault.get(1) + newline + "Twoja krzepa: " + attributsResault.get(2) + newline + "Twoja odporność: " + attributsResault.get(3) + newline + "Twoja zręczność: " + attributsResault.get(4) + newline + "Twoja inteligencja: " + attributsResault.get(5) + newline + "Twoja siła woli: " + attributsResault.get(6) + newline + "Twoja ogłada: " + attributsResault.get(7) + newline + "Twoja szybkość: " + attributsResault.get(8) + newline + "Twoja siła: " + attributsResault.get(9) + newline + "Twoja wytrzymałość: " + attributsResault.get(10) + newline + "Twoja żywotność: " + attributsResault.get(11) + newline + "Twoje punkty przeznaczenia: " + attributsResault.get(12);
+            FileWriter file = null;
+            try {
+                file = new FileWriter("Your Character M.txt");
+                file.write(writeToFile);
+            } finally {
+                if (file != null) {
+                    file.close();
+                }
+            }
 
+        }else {
+            String writeToFile = "Twoje imię to: " + femaleName + newline + "Oto wylosowane atrybuty Elfa: " + newline + "Twoja walka wręcz: " + attributsResault.get(0) + newline + "Twoje umiejętności strzeleckie: " + attributsResault.get(1) + newline + "Twoja krzepa: " + attributsResault.get(2) + newline + "Twoja odporność: " + attributsResault.get(3) + newline + "Twoja zręczność: " + attributsResault.get(4) + newline + "Twoja inteligencja: " + attributsResault.get(5) + newline + "Twoja siła woli: " + attributsResault.get(6) + newline + "Twoja ogłada: " + attributsResault.get(7) + newline + "Twoja szybkość: " + attributsResault.get(8) + newline + "Twoja siła: " + attributsResault.get(9) + newline + "Twoja wytrzymałość: " + attributsResault.get(10) + newline + "Twoja żywotność: " + attributsResault.get(11) + newline + "Twoje punkty przeznaczenia: " + attributsResault.get(12);
+            FileWriter file = null;
+            try {
+                file = new FileWriter("Your Character F.txt");
+                file.write(writeToFile);
+            } finally {
+                if (file != null) {
+                    file.close();
+                }
+            }
+        }
 
     }
 
--- src/pl/CharacterGenerator/Names.java
+++ src/pl/CharacterGenerator/Names.java
@@ -27,23 +27,19 @@ public class Names {
     public static String getFemaleName() {
 
         ArrayList<String> femaleName = new ArrayList<>();
-        femaleName.add("Aaron");
-        femaleName.add("Anna");
-        femaleName.add("Agnieszka");
-        femaleName.add("Alan");
-        femaleName.add("Albert");
-        femaleName.add("Alek");
-        femaleName.add("Aleks");
-        femaleName.add("Aleksander");
-        femaleName.add("Aleksy");
-        femaleName.add("Alex");
-        femaleName.add("Alexander");
-        femaleName.add("Alfred");
-        femaleName.add("Allan");
-        femaleName.add("Alojzy");
-        femaleName.add("Amadeusz");
+        try {
+            File file = new File("femaleName.txt");
+            Scanner sc = new Scanner(file);
+            sc.useDelimiter(",");
+            while (sc.hasNext()){
+                femaleName.add(sc.next());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         int randomIndex = ThreadLocalRandom.current().nextInt(femaleName.size());
 
         return femaleName.get(randomIndex);
+
     }
 }
