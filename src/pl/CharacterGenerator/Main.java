89c02d6c5236beb0dfdffc449b1034adc612dd95
vanyalii <adrianrozek5@gmail.com>
2020-02-16 21:39:23 +0100

    Dodanie loggerów

--- src/pl/CharacterGenerator/Main.java
+++ src/pl/CharacterGenerator/Main.java
@@ -3,9 +3,13 @@ package pl.CharacterGenerator;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
+        Logger log = Logger.getLogger("log.txt");
+
         Scanner sc = new Scanner(System.in);
 
         Race.Elf elf = new Race.Elf();
@@ -32,8 +36,7 @@ public class Main {
 
         switch (choiceSex){
             case 1:{
-
-                System.out.println("Randomowe generowanie męskiego imienia...");
+                log.log(Level.INFO, "Generowanie męskiego imienia");
                 try {
                     TimeUnit.SECONDS.sleep(3);
                 } catch (InterruptedException e) {
@@ -41,6 +44,7 @@ public class Main {
                 }
                 String maleName = Names.getMaleName();
                 System.out.println("Twoje imię to: " + maleName + newline);
+
                 break;
             }
             case 2:{
@@ -56,12 +60,14 @@ public class Main {
                 break;
             }
         }
-
+        log.log(Level.INFO,"Wybór klasy");
         System.out.println("Wybierz rase:");
         System.out.println("1: Elf");
         System.out.println("2: Człowiek");
         System.out.println("3: Krasnolud");
         int choiceRace = sc.nextInt();
+
+
 
         switch(choiceRace){
             case 1:
@@ -83,6 +89,8 @@ public class Main {
                 attributsResault.add(resistanceResault);
                 attributsResault.add(elf.ZYW());
                 attributsResault.add(elf.PP());
+
+                log.log(Level.INFO,"generowanie atrybutów");
 
                 System.out.println("Oto wylosowane atrybuty Elfa: " + newline + "Twoja walka wręcz: " + attributsResault.get(0) + newline + "Twoje umiejętności strzeleckie: " + attributsResault.get(1) + newline + "Twoja krzepa: " + attributsResault.get(2) + newline + "Twoja odporność: " + attributsResault.get(3) + newline + "Twoja zręczność: " + attributsResault.get(4) + newline + "Twoja inteligencja: " + attributsResault.get(5) + newline + "Twoja siła woli: " + attributsResault.get(6) + newline + "Twoja ogłada: " + attributsResault.get(7) + newline + "Twoja szybkość: " + attributsResault.get(8) + newline + "Twoja siła: " + attributsResault.get(9) + newline + "Twoja odporność: " + attributsResault.get(10) + newline + "Twoja żywotność: " + attributsResault.get(11) + newline + "Twoje punkty przeznaczenia: " + attributsResault.get(12));
                 break;
